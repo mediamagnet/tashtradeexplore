9da3be41b2fae908636b41e860365ffdb6790478
Nathan Conlon <nathanec@gmail.com>
2022-10-10 20:57:06 -0500

    trade art redone

--- lib/commands/trade.dart
+++ lib/commands/trade.dart
@@ -6,8 +6,14 @@ import 'package:http/http.dart' as http;
 // import 'package:tashtradeexplore/utils.dart' as utils;
 
 Future<void> loadCommand(ISlashCommandInteractionEvent event) async {
-  File file = File('./font/atkinson.zip');
-  List<int> font = file.readAsBytesSync();
+  File file1 = File('./font/atkinson.zip');
+  List<int> atkinson = file1.readAsBytesSync();
+  File file2 = File('./font/varino.zip');
+  List<int> varino = file2.readAsBytesSync();
+  File file3 = File('./font/atkinson18.zip');
+  List<int> atkinson18 = file3.readAsBytesSync();
+  File file4 = File('./font/atkinson22.zip');
+  List<int> atkinson22 = file4.readAsBytesSync();
   final tag = event.interaction.getArg('tag'); // Carrier Tag
   final name = event.interaction.getArg('name'); // Carrier name
   final ident = event.interaction.getArg('ident'); // Carrier Ident
@@ -23,15 +29,12 @@ Future<void> loadCommand(ISlashCommandInteractionEvent event) async {
   var logoImage;
   var msgText;
   var resLogo;
-
+  var baseImageFile = File('tash-ptn-mission-clone.png').uri.pathSegments.last;
+  baseImage = decodePng(File(baseImageFile).readAsBytesSync());
 
   await event.acknowledge(hidden: true);
 
-  if (bg == null) {
-    final bgImg = File('tradeDefault_bg.png').uri.pathSegments.last;
-    baseImage = decodePng(File(bgImg).readAsBytesSync());
-    resizedImage = copyResize(baseImage, width: 854);
-  } else {
+  if (bg != null) {
     print(event.interaction.resolved!.attachments
         .singleWhere((bg) => bg.id == event.interaction.getArg('bg'))
         .url);
@@ -48,27 +51,27 @@ Future<void> loadCommand(ISlashCommandInteractionEvent event) async {
     });
     final bgImg = File('tradeDl_bg.png').uri.pathSegments.last;
     print(bgImg.length);
-    baseImage = decodePng(File(bgImg).readAsBytesSync());
-    resizedImage = copyResize(baseImage, width: 854);
+    var baseImage1 = decodePng(File(bgImg).readAsBytesSync());
+    resizedImage = copyResize(baseImage1!, width: 434);
+    drawImage(baseImage, resizedImage!, dstX: 42, dstY: 12);
   }
+
 
   print('$tag, $name, $station, $system, $units, $bg, $logo');
 
-  drawString(resizedImage!, BitmapFont.fromZip(font), 0, 208,
-      'New Carrier load request for:');
-  if (tag == null) {
-    drawString(resizedImage!, BitmapFont.fromZip(font), 0, 248,
-        'Carrier: $name $ident');
+  drawString(baseImage!, BitmapFont.fromZip(varino), 546, 38,
+      'CARRIER LOADING MISSION');
+  drawString(baseImage!, BitmapFont.fromZip(atkinson18), 702, 62, ident.toString().toUpperCase());
+  if (tag != null) {
+    drawString(baseImage!, BitmapFont.fromZip(atkinson22), 546, 83, "$tag $name".toUpperCase());
   } else {
-    drawString(resizedImage!, BitmapFont.fromZip(font), 0, 248,
-        'Carrier: $tag $name $ident');
+    drawString(baseImage!, BitmapFont.fromZip(atkinson22), 546, 83, "$name".toUpperCase());
   }
-  drawString(resizedImage!, BitmapFont.fromZip(font), 0, 288,
-      'Load from: $station in $system');
-  drawString(resizedImage!, BitmapFont.fromZip(font), 0, 328,
-      'Load $units of $commodity');
-  drawString(resizedImage!, BitmapFont.fromZip(font), 0, 408,
-      'Total Profit $profit K/Unit');
+  drawString(baseImage!, BitmapFont.fromZip(atkinson), 671, 125, commodity.toString().toUpperCase());
+  drawString(baseImage!, BitmapFont.fromZip(atkinson), 671, 157, system.toString().toUpperCase());
+  drawString(baseImage!, BitmapFont.fromZip(atkinson), 671, 189, station.toString().toUpperCase());
+  drawString(baseImage!, BitmapFont.fromZip(atkinson), 671, 221,
+      '$profit k per unit, $units units');
 
   if (logo != null) {
     http
@@ -81,19 +84,25 @@ Future<void> loadCommand(ISlashCommandInteractionEvent event) async {
     final logoImg = File('tradeLogo.png').uri.pathSegments.last;
     logoImage = decodePng(File(logoImg).readAsBytesSync());
     if (logoImage!.width >= 150) {
-      resLogo = copyResize(logoImage, width: 150);
+      resLogo = copyResize(logoImage, width: 100);
       File('tradeLogo1.png').writeAsBytesSync(encodePng(resLogo));
       var resLogo1 = File('tradeLogo1.png').uri.pathSegments.last;
       logoImage = decodePng(File(resLogo1).readAsBytesSync());
     }
-    final xSize = logoImage!.width.toInt();
-    final ySize = logoImage!.height.toInt();
-    final xLoc = 854 - xSize;
-    final yLoc = 480 - ySize;
-    drawImage(resizedImage!, resLogo!, dstX: xLoc.toInt(), dstY: yLoc.toInt());
+    drawImage(baseImage!, resLogo!, dstX: 930, dstY: 13);
+  } else {
+    final logoImg = File('trade.png').uri.pathSegments.last;
+    logoImage = decodePng(File(logoImg).readAsBytesSync());
+    if (logoImage!.width >= 100) {
+      resLogo = copyResize(logoImage, width: 100);
+      File('trade.png').writeAsBytesSync(encodePng(resLogo));
+      var resLogo1 = File('trade.png').uri.pathSegments.last;
+      logoImage = decodePng(File(resLogo1).readAsBytesSync());
+    }
+    drawImage(baseImage!, resLogo!, dstX: 930, dstY: 13);
   }
 
-  File('tradeFinal.png').writeAsBytesSync(encodePng(resizedImage));
+  File('tradeFinal.png').writeAsBytesSync(encodePng(baseImage));
   String outF = File('tradeFinal.png').uri.pathSegments.last;
   print(outF);
   final attachment = AttachmentBuilder.file(File(outF));
@@ -111,8 +120,14 @@ Future<void> loadCommand(ISlashCommandInteractionEvent event) async {
 }
 
 Future<void> unloadCommand(ISlashCommandInteractionEvent event) async {
-  File file = File('./font/atkinson.zip');
-  List<int> font = file.readAsBytesSync();
+  File file1 = File('./font/atkinson.zip');
+  List<int> atkinson = file1.readAsBytesSync();
+  File file2 = File('./font/varino.zip');
+  List<int> varino = file2.readAsBytesSync();
+  File file3 = File('./font/atkinson18.zip');
+  List<int> atkinson18 = file3.readAsBytesSync();
+  File file4 = File('./font/atkinson22.zip');
+  List<int> atkinson22 = file4.readAsBytesSync();
   final tag = event.interaction.getArg('tag'); // Carrier Tag
   final name = event.interaction.getArg('name'); // Carrier name
   final ident = event.interaction.getArg('ident'); // Carrier Ident
@@ -128,14 +143,12 @@ Future<void> unloadCommand(ISlashCommandInteractionEvent event) async {
   var logoImage;
   var msgText;
   var resLogo;
+  var baseImageFile = File('tash-ptn-mission-clone.png').uri.pathSegments.last;
+  baseImage = decodePng(File(baseImageFile).readAsBytesSync());
 
   await event.acknowledge(hidden: true);
 
-  if (bg == null) {
-    final bgImg = File('tradeDefault_bg.png').uri.pathSegments.last;
-    baseImage = decodePng(File(bgImg).readAsBytesSync());
-    resizedImage = copyResize(baseImage, width: 854);
-  } else {
+  if (bg != null) {
     print(event.interaction.resolved!.attachments
         .singleWhere((bg) => bg.id == event.interaction.getArg('bg'))
         .url);
@@ -145,69 +158,75 @@ Future<void> unloadCommand(ISlashCommandInteractionEvent event) async {
         .url);
     http
         .get(Uri.parse(event.interaction.resolved!.attachments
-            .singleWhere((bg) => bg.id == event.interaction.getArg('bg'))
-            .url))
+        .singleWhere((bg) => bg.id == event.interaction.getArg('bg'))
+        .url))
         .then((response) {
       new File('tradeDl_bg.png').writeAsBytes(response.bodyBytes);
     });
     final bgImg = File('tradeDl_bg.png').uri.pathSegments.last;
     print(bgImg.length);
-    baseImage = decodePng(File(bgImg).readAsBytesSync());
-    resizedImage = copyResize(baseImage, width: 854);
+    var baseImage1 = decodePng(File(bgImg).readAsBytesSync());
+    resizedImage = copyResize(baseImage1!, width: 434);
+    drawImage(baseImage, resizedImage!, dstX: 42, dstY: 12);
   }
+
 
   print('$tag, $name, $station, $system, $units, $bg, $logo');
 
-  drawString(resizedImage!, BitmapFont.fromZip(font), 0, 208,
-      'New Carrier unload request for:');
-  if (tag == null) {
-    drawString(resizedImage!, BitmapFont.fromZip(font), 0, 248,
-        'Carrier: $name $ident');
+  drawString(baseImage!, BitmapFont.fromZip(varino), 546, 38,
+      'CARRIER UNLOADING MISSION');
+  drawString(baseImage!, BitmapFont.fromZip(atkinson18), 702, 62, ident.toString().toUpperCase());
+  if (tag != null) {
+    drawString(baseImage!, BitmapFont.fromZip(atkinson22), 546, 83, "$tag $name".toUpperCase());
   } else {
-    drawString(resizedImage!, BitmapFont.fromZip(font), 0, 248,
-        'Carrier: $tag $name $ident');
+    drawString(baseImage!, BitmapFont.fromZip(atkinson22), 546, 83, "$name".toUpperCase());
   }
-  drawString(resizedImage!, BitmapFont.fromZip(font), 0, 288,
-      'Unload to: $station in $system');
-  drawString(resizedImage!, BitmapFont.fromZip(font), 0, 328,
-      'Unload $units of $commodity');
-  drawString(resizedImage!, BitmapFont.fromZip(font), 0, 408,
-      'Total Profit $profit K/Unit');
+  drawString(baseImage!, BitmapFont.fromZip(atkinson), 671, 125, commodity.toString().toUpperCase());
+  drawString(baseImage!, BitmapFont.fromZip(atkinson), 671, 157, system.toString().toUpperCase());
+  drawString(baseImage!, BitmapFont.fromZip(atkinson), 671, 189, station.toString().toUpperCase());
+  drawString(baseImage!, BitmapFont.fromZip(atkinson), 671, 221,
+      '$profit k per unit, $units units');
 
   if (logo != null) {
     http
         .get(Uri.parse(event.interaction.resolved!.attachments
-            .singleWhere((logo) => logo.id == event.interaction.getArg('logo'))
-            .url))
+        .singleWhere((logo) => logo.id == event.interaction.getArg('logo'))
+        .url))
         .then((response) {
       new File('tradeLogo.png').writeAsBytes(response.bodyBytes);
     });
     final logoImg = File('tradeLogo.png').uri.pathSegments.last;
     logoImage = decodePng(File(logoImg).readAsBytesSync());
     if (logoImage!.width >= 150) {
-      resLogo = copyResize(logoImage, width: 150);
+      resLogo = copyResize(logoImage, width: 100);
       File('tradeLogo1.png').writeAsBytesSync(encodePng(resLogo));
       var resLogo1 = File('tradeLogo1.png').uri.pathSegments.last;
       logoImage = decodePng(File(resLogo1).readAsBytesSync());
     }
-    final xSize = logoImage!.width.toInt();
-    final ySize = logoImage!.height.toInt();
-    final xLoc = 854 - xSize;
-    final yLoc = 480 - ySize;
-    drawImage(resizedImage!, resLogo!, dstX: xLoc.toInt(), dstY: yLoc.toInt());
+    drawImage(baseImage!, resLogo!, dstX: 930, dstY: 13);
+  } else {
+    final logoImg = File('trade.png').uri.pathSegments.last;
+    logoImage = decodePng(File(logoImg).readAsBytesSync());
+    if (logoImage!.width >= 100) {
+      resLogo = copyResize(logoImage, width: 100);
+      File('trade.png').writeAsBytesSync(encodePng(resLogo));
+      var resLogo1 = File('trade.png').uri.pathSegments.last;
+      logoImage = decodePng(File(resLogo1).readAsBytesSync());
+    }
+    drawImage(baseImage!, resLogo!, dstX: 930, dstY: 13);
   }
 
-  File('tradeFinal.png').writeAsBytesSync(encodePng(resizedImage));
+  File('tradeFinal.png').writeAsBytesSync(encodePng(baseImage));
   String outF = File('tradeFinal.png').uri.pathSegments.last;
   print(outF);
   final attachment = AttachmentBuilder.file(File(outF));
 
   if (tag == null) {
     msgText =
-        '```New Unload Mission for ***$name $ident*** \nUnload ***$units*** of ***$commodity*** for ***$profit*** per unit to ***$station*** in ***$system***```';
+    '```New Load Mission for ***$name $ident*** \nUnload ***$units*** of ***$commodity*** for ***$profit*** per unit from ***$station*** in ***$system***```';
   } else {
     msgText =
-        '```New Unload Mission for ***$tag $name $ident*** \nUnload ***$units*** of ***$commodity*** for ***$profit*** per unit to ***$station*** in ***$system***```';
+    '```New Load Mission for ***$tag $name $ident*** \nUnload ***$units*** of ***$commodity*** for ***$profit*** per unit from ***$station*** in ***$system***```';
   }
 
   return event.respond(MessageBuilder.content(msgText)..files = [attachment],

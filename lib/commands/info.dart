e2cc64bc366a8169afbc674e65eb67968545e032
MediaMagnet <nathanec@gmail.com>
2022-10-17 19:12:13 +0000

    added art to ping command.

--- lib/commands/info.dart
+++ lib/commands/info.dart
@@ -24,6 +24,7 @@ Future<void> pingCommand(ISlashCommandInteractionEvent event) async {
 
   final embed = EmbedBuilder()
     ..color = color
+    ..imageUrl = 'https://i.imgur.com/pFhWc7e.gif'
     ..addField(
         name: "Gateway latency",
         content: "${gatewayDelayInMillis.abs().floor()} ms",
